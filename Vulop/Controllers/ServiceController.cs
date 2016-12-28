32437565449840d75c71f473c2f7df0c4ed5cd89
carneireiro <carneireiro@gmail.com>
2016-12-28 17:30:07 +0000

    Added new project Vulop Net Core. It will replace the previous project and will be based on .Net Core.

---
+++ Vulop/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vulop.Models;
+
+namespace Vulop.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View(new Service[] { new Models.Service() {
+                Duration = TimeSpan.FromMinutes(15),
+                Description = "Teste"
+            } });
+        }
+
+        public ActionResult ShowAdd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vulop/Models/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vulop.Models
+{
+    public class Service
+    {
+        [Display(Name = "Descrição")]
+        public string Description { get; set; }
+
+        [Display(Name = "Duração")]
+        public TimeSpan Duration { get; set; }
+    }
+}
